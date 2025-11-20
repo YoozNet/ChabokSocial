66deb2a68ef517352a040b632e66e5222f9cb00d
Golden-Source <mail@goldensource.ir>
2025-11-20 13:08:38 +0330

    fix bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,14 +3,16 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use App\Http\Middleware\{
+    InstallGuard,
+};
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(InstallGuard::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
